889980959e67985b23b214c6eceafcee8bd7486b
Sarapena02 <94878936+Sarapena02@users.noreply.github.com>
2023-04-11 20:34:10 -0500

    se realiza la funcion verClases en ClaseController para mostrar las clases de la asignatura seleccionada por el usuario.

--- zip/src/main/java/com/proyecto/model/repository/NotaRepository.java
+++ zip/src/main/java/com/proyecto/model/repository/NotaRepository.java
@@ -1,11 +1,20 @@
 package com.proyecto.model.repository;
 
+import com.proyecto.model.entity.Clase;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.proyecto.model.entity.Nota;
 
+import java.util.List;
+
 @Repository
 public interface NotaRepository extends CrudRepository<Nota,Long>{
-    
+    @Query("SELECT n FROM Nota" +
+            "JOIN n.listaNotas ln" +
+            "JOIN ln.clase" +
+            "WHERE c.id = :claseId")
+    List<Nota> getNotas(@Param("Clase") Clase clase);
 }
