8841cca382fe8549cd0c1b4b97cccd410de1147a
Daniel Florido <d.florido@gmail.com>
2023-05-02 20:54:02 -0500

    fix bugs

---
+++ zip/src/main/java/com/proyecto/control/HomeController.java
@@ -0,0 +1,12 @@
+package com.proyecto.control;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String redireccion(){
+        return "index";
+    }
+}
